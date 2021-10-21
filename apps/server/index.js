eb52c4676543402e7fad2762b7f4d3f8fad0ab11
uriel-s <57845016+uriel-s@users.noreply.github.com>
2021-10-22 00:52:53 +0300

    make it better

---
+++ apps/server/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bodyParser =require('body-parser')
+const cors = require('cors');
+const user_route = require('./routes/users');
+const PORT = process.env.PORT || 5000
+const app = express();
+const { initializeApp,  cert } = require('firebase-admin/app');
+const { getFirestore} = require('firebase-admin/firestore');
+const { doc, getDoc ,collection}  = require( "firebase/firestore");
+const serviceAccount = require('./serviceAccountKey');
+
+
+require("dotenv/config");
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+
+//connect firestore with serviceAccountKey details
+initializeApp({
+      credential: cert(serviceAccount)
+});
+    
+const db = getFirestore();
+console.log("connect to db")
+
+//routrs
+app.post('/add'        ,(req, res) =>  { user_route.handleRegister(req, res, db, ) })
+app.get('/users/:email',(req, res) => {user_route.getUserbyEmail(req, res, db, )})
+app.put('/update/:id',(req, res) => {user_route.updateUser(req, res, db, )})
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server runing on port ${PORT}`)});
+
+
+
+
+
+
+
+
+    
+// make all vallidation with frontend and firebase no need other validation
+
+
+    //const validRegister = () =>
+//[
+  //  check('email' ,'Email is not valid' ).isEmail().normalizeEmail(),
+    //check('name', 'Name is required').notEmpty(),
+    // check('password',"password is not valid").isLength({ min: 6, }).matches(
+    // /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]/ 
+    // ) 
+//] 
---
+++ apps/server/routes/users.js
@@ -0,0 +1,75 @@
+
+
+// @route    GET /users/:id
+// @desc     Get current user profile
+// @access   Private
+const getUserbyEmail = async (req ,res ,db ,) =>{
+  const  email = await req.params.email
+  try { 
+    const query = await db.collection('users').where('email', '==', email).get()
+    if (query.empty) {
+        console.log("user not exist")
+        return res.status(500).send("user not exist");
+      }
+    const snapshot = query.docs[0];
+    const user = snapshot.data();
+    res.json(user)
+  }
+  catch
+  {
+    return res.status(500).send("email not exist");
+  }
+
+}
+// @route    put /users/:email
+// @desc     update current users profile
+// @access   Private
+const updateUser = async (req ,res ,db ,) =>{
+  const  fireBaseid = await req.params.id
+  const { email, name,  birthDate, address} = await req.body;
+  try
+  {
+    const docRef =  db.collection('users').doc(fireBaseid);
+    var user = docRef.update({
+      email: email,
+      name : name, 
+      birthDate : birthDate,
+      address : address
+    });
+    res.json("succses")
+  }
+  catch
+  {
+    return res.status(500).send("can not update");
+  }
+
+}
+
+
+// @route    POST users/profile
+// @desc     Create  user profile
+// @access   Private
+const  handleRegister = async (req, res, db,  ) => {
+  const { email, name, id ,birthDate,address} = await req.body;
+  try{
+      const docRef =  db.collection('users').doc(id);
+      const newUser =  docRef.set({
+        id :id,
+        name: name,
+        birthDate: birthDate,
+        email: email,
+        address : address,
+        });
+        res.json("success");
+      }
+  catch{
+    return res.status(500).send("can not register");
+  }
+
+}
+
+module.exports = {
+    handleRegister: handleRegister,
+    getUserbyEmail : getUserbyEmail,
+    updateUser :  updateUser
+  };
